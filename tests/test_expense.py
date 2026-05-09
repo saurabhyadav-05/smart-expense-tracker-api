6abedfd4ec9e97c7334897cf125392eb61520e48
SAURABH <saudyad42@gmail.com>
2026-05-09 14:29:12 +0530

    Fixed pytest import issue

---
+++ tests/test_expense.py
@@ -0,0 +1,23 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_home():
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+
+
+def test_get_expenses():
+
+    response = client.get("/expenses")
+
+    assert response.status_code == 200
